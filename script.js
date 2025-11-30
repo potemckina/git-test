5aa0c4bd641ed8ccd84d41f983ab601d04604f15
nina <you@example.com>
2025-11-30 14:51:35 +0300

    function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 alert('привет из внешнего файла!')
+
+function changeContent() {
+    document.getElementById("title").innerHTML = "hello javascript!";
+}
+
+function changeImage() {
+    document.getElementById("image").src = "./images/project-arrival-dashboard.png";
+    document.getElementById("image").alt = "image 2";
+}
+
+function changeStyle() {
+    document.getElementById("text").style.color = "pink";
+    document.getElementById("text").style.fontSize = "50px";
+}
+function toggleHighlight() {
+    document.getElementById("text-2").classList.toggle("highlight");
+}
