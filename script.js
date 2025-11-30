73fd53b5dadc09798916a26cc31f3c3dfdecd233
nina <you@example.com>
2025-11-30 15:36:58 +0300

    + homework

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ function changeStyle() {
 function toggleHighlight() {
     document.getElementById("text-2").classList.toggle("highlight");
 }
+
+
+// homework example 1
+const myCity = 'saint-petersburg,';
+let temperature = 2;
+console.log(myCity, temperature);
+
+//example 2
+const result = ((2+7) * 15);
+console.log(result);
+
+// в первом примере городу задаю конст потому что город у нас неизменный, а в лет задаю градусы, потому что можно переопределить, в консоль вывожу название города и количество градусов в нем
+// во втором примере пишу через конст но можно и через лет задать выражение и его итог вывожу в консоль 
